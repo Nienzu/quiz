516166f81bb5fc243ca4c2f8199d77afba36ab7a
Tommy <MacWu@TommydeMacBook-Pro.local>
2015-10-23 20:45:21 +0800

    Complete iteration version & Add number_gen.
    number_gen generate test file

--- q4/iterative.c
+++ q4/iterative.c
@@ -1,6 +1,38 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_ARR 9
+
+int maxSubArray(int A[]);
 
 int main()
 {
+    FILE *fp;
+    fp = fopen("test.txt", "r");
+    int A[MAX_ARR];
+    int i;
+    for(i = 0; i < MAX_ARR; i++)
+        fscanf(fp, "%d", &A[i]);
+    fclose(fp);
+    int max;
+    max = maxSubArray(A);
+    printf("%d\n", max);
     return 0;
+}
+
+int maxSubArray(int A[])
+{
+    int i, j, k;
+    int temp = 0;
+    int sum = 0;
+    for(i = 0; i < MAX_ARR; i++) {
+        for(j = 0; j < (MAX_ARR - i); j++)
+            for(k = j; k < j+i; k++)
+                temp +=A[k];
+        if(temp > sum)
+            sum = temp;
+        temp = 0;
+    }
+
+    return sum;
 }
