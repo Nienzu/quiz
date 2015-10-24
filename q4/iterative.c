f88641ce6a7fb90216580b11f0064a9a033b7902
nienzu <ibqqz0602@gmail.com>
2015-10-25 00:09:04 +0800

    Make some change the algorithm of maxSubArray.
    The first version is wrong, and I change it.

--- q4/iterative.c
+++ q4/iterative.c
@@ -1,37 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
-#define MAX_ARR 9
+#define MAX_ARR 100
 
-int maxSubArray(int A[]);
+int maxSubArray(int A[], int size);
 
 int main()
 {
     FILE *fp;
     fp = fopen("test.txt", "r");
     int A[MAX_ARR];
-    int i;
-    for(i = 0; i < MAX_ARR; i++)
-        fscanf(fp, "%d", &A[i]);
+    int i = 0;
+    while(fscanf(fp, "%d", &A[i]) != EOF) {
+        printf("%d ", A[i]);
+        i++;
+    }
     fclose(fp);
     int max;
-    max = maxSubArray(A);
+    printf("\n%d\n", i);
+    max = maxSubArray(A, i);
     printf("%d\n", max);
     return 0;
 }
 
-int maxSubArray(int A[])
+int maxSubArray(int A[], int size)
 {
     int i, j, k;
     int temp = 0;
-    int sum = 0;
-    for(i = 0; i < MAX_ARR; i++) {
-        for(j = 0; j < (MAX_ARR - i); j++)
-            for(k = j; k < j+i; k++)
-                temp +=A[k];
-        if(temp > sum)
-            sum = temp;
-        temp = 0;
+    int sum = INT_MIN;
+    for(i = 0; i < size; i++) {
+        for(j = 0; j < size - i; j++) {
+            for(k = j; k < j + i; k++)
+                temp += A[k];
+            if(temp > sum)
+                sum = temp;
+            printf("%d ", temp);
+            temp = 0;
+
+        }
     }
 
     return sum;
